b49bc4f07440dc685ac225a0722562ddd54e902f
seba <komorowski.seba@gmail.com>
2022-06-06 00:22:35 +0200

    logs

--- Infrastructure/Elastic/ElasticExtensions.cs
+++ Infrastructure/Elastic/ElasticExtensions.cs
@@ -1,7 +1,10 @@
-﻿using Application.Interfaces;
+﻿using System.Reflection;
+using Application.Interfaces;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Nest;
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
 
 namespace Infrastructure.Elastic;
 
@@ -16,5 +19,38 @@ public static class ElasticExtensions
         // services.AddSingleton<IElasticClient>(client);            
         // services.AddScoped<ISearchService, ElasticSearchService>();
         return services;
+    }
+
+    public static void SetConfigureLogging(IConfiguration configuration)
+    {
+        var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+        // var configuration = new ConfigurationBuilder()
+        //     .AddJsonFile(
+        //         "appsettings.json", 
+        //         optional: false, 
+        //         reloadOnChange: true)
+        //     .AddJsonFile(
+        //         $"appsettings.{environment}.json",
+        //         optional: true)
+        //     .Build();
+        
+        Log.Logger = new LoggerConfiguration()
+            .Enrich.FromLogContext()
+            .Enrich.WithMachineName()
+            .WriteTo.Debug()
+            .WriteTo.Console()
+            .WriteTo.Elasticsearch(ConfigureElasticSink(configuration, environment))
+            .Enrich.WithProperty("Environment", environment)
+            .ReadFrom.Configuration(configuration)
+            .CreateLogger();
+    }
+    
+    private static ElasticsearchSinkOptions ConfigureElasticSink(IConfigurationRoot configuration, string environment)
+    {
+        return new ElasticsearchSinkOptions(new Uri(configuration["Elasticsearch:Url"]))
+        {
+            AutoRegisterTemplate = true,
+            IndexFormat = $"{Assembly.GetExecutingAssembly().GetName().Name.ToLower().Replace(".", "-")}-{environment?.ToLower().Replace(".", "-")}-{DateTime.UtcNow:yyyy-MM}"
+        };
     }
 }
