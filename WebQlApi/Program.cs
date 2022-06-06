13c22487f7a149f500d78faab964c1c6f713df3f
seba <komorowski.seba@gmail.com>
2022-06-07 00:22:52 +0200

    serilog next

--- WebQlApi/Program.cs
+++ WebQlApi/Program.cs
@@ -5,8 +5,28 @@ using Infrastructure;
 using Infrastructure.Persistence;
 using Infrastructure.QL;
 using InternalEvents;
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Host.UseSerilog((context, configuration) =>
+{
+    configuration
+        .WriteTo.Console()
+        .Enrich.FromLogContext()
+        .Enrich.WithMachineName()
+        .WriteTo.Elasticsearch(
+            new ElasticsearchSinkOptions(new Uri(""))
+            {
+                IndexFormat = "",//$"{Assembly.GetExecutingAssembly().GetName().Name.ToLower().Replace(".", "-")}-{environment?.ToLower().Replace(".", "-")}-{DateTime.UtcNow:yyyy-MM}"
+                AutoRegisterTemplate = true,
+                NumberOfShards = 2,
+                NumberOfReplicas = 1
+            })
+        .Enrich.WithProperty("Environment", "")
+        .ReadFrom.Configuration("context configuration")
+});
+
 var services = builder.Services;
 services.AddControllers();
 services.AddEndpointsApiExplorer();
