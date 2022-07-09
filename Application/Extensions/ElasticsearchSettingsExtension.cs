3be38dc5c93bc6613c157a0f486b31e831c95911
seba <komorowski.seba@gmail.com>
2022-07-09 23:06:10 +0200

    added elasticsearch & Redis settings extensions

---
+++ Application/Extensions/ElasticsearchSettingsExtension.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Application.Extensions;
+
+public static class ElasticsearchSettingsExtension
+{
+    public static string GetSettingsElasticsearchIndex(this IConfiguration configuration)
+        => configuration.GetValue<string>("Elasticsearch:Index");
+
+    public static string GetSettingsElasticsearchUrl(this IConfiguration configuration)
+        => configuration.GetValue<string>("Elasticsearch:Url");
+}
---
+++ Application/Extensions/RedisSettingsExtension.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Application.Extensions;
+
+public static class RedisSettingsExtension
+{
+    public static string GetSettingsRedisConfigurationHost(this IConfiguration configuration)
+        => configuration.GetValue<string>("Redis:ConfigurationHost");
+
+    public static string GetSettingsRedisInstanceName(this IConfiguration configuration)
+        => configuration.GetValue<string>("Redis:InstanceName");
+}
