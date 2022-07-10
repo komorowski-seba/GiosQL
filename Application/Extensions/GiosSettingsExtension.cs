423253364e149072e9dd228e306000bd7a5c1abc
seba <komorowski.seba@gmail.com>
2022-07-10 23:51:02 +0200

    Gios settings

---
+++ Application/Extensions/GiosSettingsExtension.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Application.Extensions;
+
+public static class GiosSettingsExtension
+{
+    public static string GetSettingsGiosStationsUrl(this IConfiguration configuration)
+        => configuration.GetValue<string>("Gios:StationsUrl");
+
+    public static string GetSettingsGiosQualityUrl(this IConfiguration configuration)
+        => configuration.GetValue<string>("Gios:QualityUrl");
+}
